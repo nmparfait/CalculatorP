ae9c70dc2958cb4d3e273dd2609b8936dfe25961
nmparfait <mbamu.developer@gmail.com>
2021-01-29 19:37:17 +0000

    printing the logo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from art import logo 
-print(logo)
+
+from art import logo
 
 #some functions of our project
 
@@ -29,6 +29,7 @@ operations = {
 }
 
 def calculator():
+  print(logo)
   #getting the first number
   n1 = int(input("What is the first number?: "))
 
@@ -42,26 +43,22 @@ def calculator():
       
     #operations symbol 
     operation_symbol = input("Pick an operation from the line above:")
-
     #getting the second number
     n2 = int(input("What is the second number?: "))
-
     calculation_function = operations[operation_symbol]
     answer = calculation_function(n1, n2)
 
-
     print(f"{n1} {operation_symbol} {n2} = {answer}" )
-
     #making another operation
     operation_symbol = input("Pick another operation:")
-
-    
-
     if input(f"Type 'y' to continue calculating with {answer} or type 'no' to start a new calculation: ") == "y":
       n1 == answer
     else:
       should_continue = False
       calculator()
+
+
+calculator()
       
 
 
