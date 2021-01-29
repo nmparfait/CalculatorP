b057849fd457924e40bc95cd217c0d77cb853990
nmparfait <mbamu.developer@gmail.com>
2021-01-29 18:24:16 +0000

    Restarting a game with the function

--- main.py
+++ main.py
@@ -27,38 +27,41 @@ operations = {
   "/": division
 }
 
-#getting the first number
-n1 = int(input("What is the first number?: "))
+def calculator():
+  #getting the first number
+  n1 = int(input("What is the first number?: "))
 
-#loop through the dictionnary
-for symbol in operations:
-  print(symbol)
+  #loop through the dictionnary
+  for symbol in operations:
+    print(symbol)
 
-should_continue = True
+  should_continue = True
 
-while should_continue:
+  while should_continue:
+      
+    #operations symbol 
+    operation_symbol = input("Pick an operation from the line above:")
+
+    #getting the second number
+    n2 = int(input("What is the second number?: "))
+
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(n1, n2)
+
+
+    print(f"{n1} {operation_symbol} {n2} = {answer}" )
+
+    #making another operation
+    operation_symbol = input("Pick another operation:")
+
     
-  #operations symbol 
-  operation_symbol = input("Pick an operation from the line above:")
 
-  #getting the second number
-  n2 = int(input("What is the second number?: "))
-
-  calculation_function = operations[operation_symbol]
-  answer = calculation_function(n1, n2)
-
-
-  print(f"{n1} {operation_symbol} {n2} = {answer}" )
-
-  #making another operation
-  operation_symbol = input("Pick another operation:")
-
-  
-
-  if input(f"Type 'y' to continue calculating with {answer} or type 'no' to exit: ") == "y":
-    n1 == answer
-  else:
-    should_continue = False
+    if input(f"Type 'y' to continue calculating with {answer} or type 'no' to start a new calculation: ") == "y":
+      n1 == answer
+    else:
+      should_continue = False
+      calculator()
+      
 
 
 
